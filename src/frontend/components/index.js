632817bfaf9b01eb60bfe456d5cea8a6db471846
Srujana Penugonda <srujanapenugonda1318@outlook.com>
2022-04-18 07:33:04 +0530

    Refactor - updated folder names

--- src/frontend/components/index.js
+++ src/frontend/components/index.js
@@ -1,7 +1,7 @@
 export { Navbar } from "./navbar/Navbar";
 export { Footer } from "./footer/Footer";
-export { Categories } from "./categories/Categories";
-export { Welcome } from "./welcome/Welcome";
+export { Categories } from "./Categories/Categories";
+export { Welcome } from "./Welcome/Welcome";
 export { Filters } from "./filters/Filters";
 export { ProductCard } from "./productCard/ProductCard";
 export { ProductList } from "./productsList/ProductList";
