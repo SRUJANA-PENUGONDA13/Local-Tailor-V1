864fa1b37fc3767b79a8c0d4c597d9e01b33a614
Srujana Penugonda <srujanapenugonda1318@outlook.com>
2022-05-01 18:49:55 +0530

    Fix - Added categories functionality in home page

--- src/frontend/components/Categories/Categories.jsx
+++ src/frontend/components/Categories/Categories.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { categories } from "../../../backend/db/categories";
+import { useProduct } from "../../context";
+import { getCategorizedProducts } from "../../utils/productFilters";
 import "./Categories.css";
 
 const Categories = () => {
+  const [{ products }, productDispatch] = useProduct();
+
+  const categoryActionHandler = (categorie) => {
+    productDispatch({
+      type: "INCLUDE_CATEGORY",
+      payload: categorie.toLowerCase(),
+    });
+  };
+
   return (
     <div className="featured-categories flex-dir-col">
       <h1>Featured Categories</h1>
@@ -15,7 +26,12 @@ const Categories = () => {
           categories.map((categorie) => {
             return (
               <p className="categorie">
-                <Link className="text-decoration-none" to="/products">
+                <Link
+                  className="text-decoration-none"
+                  to="/products"
+                  value={categorie}
+                  onClick={(event) => categoryActionHandler(event.target.text)}
+                >
                   {categorie.categoryName}
                 </Link>
               </p>
